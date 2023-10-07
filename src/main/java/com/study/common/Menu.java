ab048a6fe6942ae7db9a4c34f479afa3bb9da90d
FYB <849854223@qq.com>
2023-10-07 11:07:55 +0800

    2023/10/7 menu

---
+++ src/main/java/com/study/common/Menu.java
@@ -0,0 +1,16 @@
+package com.study.common;
+
+import java.util.Map;
+import java.util.HashMap;
+
+public class Menu {
+
+	// TODO
+	/** 存款 */
+	public static final String DEPOSIT_URL = "/web/depositPage";
+	/** 取款 */
+	public static final String WITHDRAW_URL = "/web/withdrawPage";
+	/** 查看账户余额 */
+	public static final String GET_BALANCE_URL = "";
+
+}
